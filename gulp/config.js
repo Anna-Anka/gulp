941c4428061af86fd228128ed50a68d7402b2a5c
Anna-Anka <anjbekrenewa@gmail.com>
2024-08-15 20:39:52 +0300

    refactor paths

--- gulp/config.js
+++ gulp/config.js
@@ -34,43 +34,46 @@ export const plugins = {
     gulp, debug, deleteAsync, imagemin, imageminPngquant, svgo, imageminZopfli, imageminMozjpeg, imageminGiflossy, newer, browsersync, webpack, webpackStream, gulpif, svg, gulpsass, dartsass, mincss, groupmedia, autoprefixer, sourcemaps, plumber, include, webp, ttf2woff, ttf2woff2, path, notify, zip, avif, deploy, filter,
 };
 
+const devFolder = './src';
+const productFolder = './app';
+
 export const paths = {
     html: {
-        src: './src/html/*.html',
-        app: './app/',
-        watch: './src/html/**/*.html',
+        src: `${devFolder}/html/*.html`,
+        app: `${productFolder}/`,
+        watch: `${devFolder}/html/**/*.html`,
     },
     styles: {
-        src: './src/styles/style.{scss,sass,css}',
-        app: './app/css/',
-        watch: './src/styles/**/*.{scss,sass,css}',
+        src: `${devFolder}/styles/style.{scss,sass,css}`,
+        app: `${productFolder}/css/`,
+        watch: `${devFolder}/styles/**/*.{scss,sass,css}`,
     },
     scripts: {
-        src: './src/js/main.js',
-        app: './app/js/',
-        watch: './src/js/**/*.js',
+        src: `${devFolder}/js/main.js`,
+        app: `${productFolder}/js/`,
+        watch: `${devFolder}/js/**/*.js`,
     },
     images: {
-        src: './src/img',
-        app: './app/img',
-        watch: './src/img/',
-        srcExceptions: ['!./src/img/sprites/**/*']
+        src: `${devFolder}/img`,
+        app: `${productFolder}/img`,
+        watch: `${devFolder}/img/`,
+        srcExceptions: [`!${devFolder}/img/sprites/**/*`]
     },
     sprites: {
-        src: './src/img/sprites/*.svg',
-        app: './app/img/sprites/',
-        watch: './src/img/sprites/*.svg',
+        src: `${devFolder}/img/sprites/*.svg`,
+        app: `${productFolder}/img/sprites/`,
+        watch: `${devFolder}/img/sprites/*.svg`,
     },
     fonts: {
-        src: './src/fonts/**/*',
-        app: './app/fonts/',
-        watch: './src/fonts/**/*',
+        src: `${devFolder}/fonts/**/*`,
+        app: `${productFolder}/fonts/`,
+        watch: `${devFolder}/fonts/**/*`,
     },
     resources: {
-        src: './src/resources/**'
+        src: `${devFolder}/resources/**`
     },
-    devFolder: './src',
-    productFolder: './app',
+    devFolder,
+    productFolder,
     rootFolder: path.basename(path.resolve()),
 };
 
